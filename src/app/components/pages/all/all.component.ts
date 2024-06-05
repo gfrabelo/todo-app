218959ca052391699669e7f6d8dddbdec49260b9
gfrabelo <gabrielrabelob@gmail.com>
2024-06-04 23:37:18 -0300

    feat: atualiza Json

--- src/app/components/pages/all/all.component.ts
+++ src/app/components/pages/all/all.component.ts
@@ -1,12 +1,11 @@
 import { Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { DateComponent } from '../../date/date.component';
 import { TaskListComponent } from '../../task-list/task-list.component';
 
 @Component({
   selector: 'app-all',
   standalone: true,
-  imports: [FormsModule, DateComponent, TaskListComponent],
+  imports: [DateComponent, TaskListComponent],
   templateUrl: './all.component.html',
   styles: ''
 })
