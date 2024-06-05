45e29c9dea3778aa3a2073c8648278df5c521ab1
gfrabelo <gabrielrabelob@gmail.com>
2024-06-04 22:46:31 -0300

    feat: atualiza data dinamicamente

--- src/app/components/pages/all/all.component.ts
+++ src/app/components/pages/all/all.component.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from '@angular/common';
 import { HttpService } from './../../../services/http.service';
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -5,14 +6,15 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-all',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, DatePipe],
   templateUrl: './all.component.html',
   styles: ''
 })
 export class AllComponent {
   newTask = "";
   taskList:any = []=[];
-  HttpService=inject(HttpService);
+  HttpService = inject(HttpService);
+  dateNow = new Date();
   ngOnInit (){
     this.getAllTasks();
   }
