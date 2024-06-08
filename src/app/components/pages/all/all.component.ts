2964f51ef9c6d8b19d44da87868b1e3a3977fda4
gfrabelo <gabrielrabelob@gmail.com>
2024-06-08 18:21:05 -0300

    feat: altera rota [Completed]

--- src/app/components/pages/all/all.component.ts
+++ src/app/components/pages/all/all.component.ts
@@ -1,24 +1,45 @@
 import { HttpService } from './../../../services/http.service';
-import { Component, inject } from '@angular/core';
+import { Component, EventEmitter, Output, inject } from '@angular/core';
 import { DateComponent } from '../../date/date.component';
 import { TaskListComponent } from '../../task-list/task-list.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-all',
   standalone: true,
-  imports: [DateComponent, TaskListComponent],
+  imports: [DateComponent, TaskListComponent, FormsModule],
   templateUrl: './all.component.html',
   styles: ''
 })
 export class AllComponent {
 
+  newTask = "";
+  taskList:any = []=[];
   HttpService = inject(HttpService);
-
+  ngOnInit (){
+    this.getAllTasks();
+  }
+  addTask(){
+    this.HttpService.addTask(this.newTask).subscribe(() => {
+      this.newTask="";
+      this.getAllTasks()
+    })
+  }
+  getAllTasks(){
+    this.HttpService.getAllTasks().subscribe((result:any) => {
+      this.taskList = result
+    })
+  }
   onComplete(task:any){
     task.closed = true;
-    console.log("tatenu", task)
     this.HttpService.updateTask(task).subscribe(() => {
-
+      this.getAllTasks();
     })
+  }
+
+  @Output() closed = new EventEmitter<any>();
+
+  markClosed(task:any) {
+    this.closed.emit(task);
   }
 }
--- src/app/components/pages/completed/completed.component.ts
+++ src/app/components/pages/completed/completed.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DateComponent } from '../../date/date.component';
+import { TaskListComponent } from '../../task-list/task-list.component';
+import { HttpService } from '../../../services/http.service';
 
 @Component({
   selector: 'app-completed',
   standalone: true,
-  imports: [],
+  imports: [DateComponent, TaskListComponent ],
   templateUrl: './completed.component.html',
-  styleUrl: './completed.component.scss'
+  styles: ''
 })
 export class CompletedComponent {
-
+  newTask = "";
+  taskList:any = []=[];
+  HttpService = inject(HttpService);
+  ngOnInit (){
+    this.getAllTasks();
+  }
+  getAllTasks(){
+    this.HttpService.getAllTasks().subscribe((result:any) => {
+      this.taskList = result.filter((x:any) => x.closed == true);
+    })
+  }
+  onComplete(task:any){
+    task.closed = true;
+    this.HttpService.updateTask(task).subscribe(() => {
+      this.getAllTasks();
+    })
+  }
 }
