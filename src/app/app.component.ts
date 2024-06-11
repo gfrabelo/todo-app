d8bbe7ad444fff65fced928f812de1c743ce4f55
gfrabelo <gabrielrabelob@gmail.com>
2024-06-10 22:30:52 -0300

    fix: ajusta margin left

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { AsideComponent } from './components/aside/aside.component';
 import { TaskListComponent } from "./components/task-list/task-list.component";
 
 @Component({
@@ -9,7 +7,7 @@ import { TaskListComponent } from "./components/task-list/task-list.component";
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HeaderComponent, AsideComponent, TaskListComponent]
+    imports: [HeaderComponent, TaskListComponent]
 })
 export class AppComponent {
   title = 'todo-app';
