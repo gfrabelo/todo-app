0552cd837bcb3e9c4114d7e93630366220f6df2b
gfrabelo <gabrielrabelob@gmail.com>
2024-06-11 23:51:03 -0300

    feat: adiciona search task

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './header.component.html',
   styles: ''
 })
 export class HeaderComponent {
-
+  stateService = inject(StateService);
+  searchControl = new FormControl('');
+  ngOnInit () {
+    this.searchControl.valueChanges.pipe(debounceTime(250)).subscribe((value) => {
+      this.stateService.searchSubject.next(value || '')
+    });
+  }
 }
